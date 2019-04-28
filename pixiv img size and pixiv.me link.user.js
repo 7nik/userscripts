f81e3d5159cca48166a0465344d1e591002fbad6
7nik <kfiiranet@gmail.com>
2019-04-28 23:29:18 +0300

    version increment

--- pixiv img size and pixiv.me link.user.js
+++ pixiv img size and pixiv.me link.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pixiv img size and pixiv.me link
 // @namespace    http://tampermonkey.net/
-// @version      1.2.2
+// @version      1.2.3
 // @description  Add size of the illustrations and direct links to them.
 // @author       7nik
 // @match        https://www.pixiv.net/member_illust.php*
