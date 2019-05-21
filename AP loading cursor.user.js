d389ce299ec0593290ee7cf20fcec4cb0f0d8f4e
7nik <kfiiranet@gmail.com>
2019-05-21 22:48:36 +0300

    refactoring

--- AP loading cursor.user.js
+++ AP loading cursor.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AP loading cursor
 // @namespace    7nik@anime-pictures.net
-// @version      0.1.2
+// @version      0.1.3
 // @description  Set "progress" cursor during executing long queries to the server
 // @author       7nik
 // @match        https://anime-pictures.net/*
@@ -18,7 +18,7 @@
 
     let timer = null;
     ajax_request2 = function(url, params, handler, type_r) {
-        if (timer) clearTimeout(timer);
+        if (timer) timer = clearTimeout(timer);
         timer = setTimeout(() => document.body.classList.add("waiting"), 500);
         if (type_r == null) {
             type_r = "POST";
@@ -30,7 +30,7 @@
         var request = new XMLHttpRequest();
         request.open(type_r, url);
         request.onreadystatechange = function() {
-            if (timer) clearTimeout(timer);
+            if (timer) timer = clearTimeout(timer);
             document.body.classList.remove("waiting");
             handler(request);
         };
