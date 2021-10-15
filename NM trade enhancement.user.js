cb2db352cc825d973cbc8ce9d346443ecb0cf14d
7nik <kifiranet@gmail.com>
2021-10-15 16:36:58 +0300

    version increment

--- NM trade enhancement.user.js
+++ NM trade enhancement.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NM trade enhancement
 // @namespace    7nik
-// @version      2.0.1
+// @version      2.1.0
 // @description  Adds enhancements to the trading window
 // @author       7nik
 // @homepageURL  https://github.com/7nik/userscripts
